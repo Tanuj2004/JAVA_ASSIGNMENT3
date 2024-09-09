6f0b6eb3ab420c42dc30aab128d8108333312f1a
Tanuj2004 <22egjcs811@gitjaipur.com>
2024-09-09 15:33:05 +0530

    Add files via upload

---
+++ Errors.java
@@ -0,0 +1,11 @@
+/**
+ * This class contains a set of error string that are used throughout the program to display errors .
+ * Owner-->TANUJ YADAV
+ * Date-->09/09/2024
+ */
+public final class Errors {
+
+    public static final String INPUTERROR = "Invalid input. Please enter a number between 1 and 5.";
+    public static final String INVALIDTASKNUMBER = "Invalid task number. Please enter a number between 1 and 5.";
+    public static final String COMBINATIONERROR =  "No combinations of consecutive natural numbers found for ";
+}
